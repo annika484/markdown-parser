89c849388c9b21c347038aae53e4f65a7d0062ed
annika484 <aaorourke@ucsd.edu>
2022-04-24 21:28:42 -0700

    Fixed symptom #1

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,10 +11,14 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if(openBracket==-1){
+                break;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
